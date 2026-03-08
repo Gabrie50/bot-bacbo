ea7ae7fe8e7a070c880a849e9e5e7ae8047b2cdf
Gabrie50 <gcriste268@gmail.com>
2026-03-08 11:54:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ import pg8000
 # =============================================================================
 # CONFIGURAÇÕES
 # =============================================================================
-DATABASE_URL = "postgresql://neondb_owner:npg_ukM0UVmB7vwK@ep-cold-bar-a47x21jb-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DATABASE_URL = "postgresql://neondb_owner:npg_6qPXcm8FzOHn@ep-young-snow-acgrqigh-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 # Parse da URL
 parsed = urllib.parse.urlparse(DATABASE_URL)
 DB_USER = parsed.username
